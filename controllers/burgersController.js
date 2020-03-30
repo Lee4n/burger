03ffd423022d186c8d24541ee24222ff7205a0b5
Lee Lawler <lawler_lee@icloud.com>
2020-03-29 20:58:19 -0500

    added orm object/controller routes

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -10,10 +10,22 @@ router.get("/", function(req, res) {
 
 router.get("/burgers", function(req, res) {
   //FIXME:
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  })
 });
 
 // post route -> back to index
-
+router.post("/burgers", function(req, res) {
+  burger.createOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    res.json(result)
+    res.redirect("/burgers")
+  })
+});
 
 // put route -> back to index
 
