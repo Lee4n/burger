828a86afe39bf2e79a07acbf07ff132d9738487e
Lee Lawler <lawler_lee@icloud.com>
2020-03-31 21:01:39 -0500

    get method working

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -4,24 +4,22 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 // get route -> index
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.redirect("/burgers");
 });
 
-router.get("/burgers", function(req, res) {
+router.get("/burgers", function (req, res) {
   //FIXME:
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+  burger.all(function (data) {
+    res.render("index", {
+      burger_data: data
+    });
   })
 });
 
 // post route -> back to index
-router.post("/burgers", function(req, res) {
-  burger.createOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+router.post("/api/burgers", function (req, res) {
+  burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
     res.json(result)
     res.redirect("/burgers")
   })
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-    
-  $(".devour-form").on("submit", function(event) {
+$(document).ready(function () {
+
+  $(".devour-form").on("submit", function (event) {
     event.preventDefault();
 
     var burger_id = $(this).children(".burger_id").val();
@@ -8,7 +8,7 @@ $(document).ready(function() {
     $.ajax({
       method: "PUT",
       url: "/burgers/" + burger_id
-    }).then(function(data) {
+    }).then(function (data) {
       // reload page to display devoured burger in proper column
       location.reload();
     });
