14177a4b0369b8517472fc25de698976b679fead
Lee Lawler <lawler_lee@icloud.com>
2020-04-04 20:28:17 -0500

    added JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: 3306,
-  user: process.env.USER_DB,
-  password: process.env.USER_PASS,
-  database: "burgers_db"
-});
-
-
+require("dotenv").config();
+var connection;
+if( process.env.JAWSDB_URL ){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: "burgers_db"
+  });
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -16,5 +20,4 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
 module.exports = connection;
